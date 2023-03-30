d314bd1bcf3edec7662b3f907c86ada99b5036e9
AdamMakoun <adammakaeu@gmail.com>
2023-03-30 12:23:01 +0200

    Adding Project

---
+++ Guns/Guns/Mp5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guns
+{
+    class Mp5 : Guns
+    {
+        public Mp5 (string name, int base_ammo, int ammount_mag, string img) : base(name, base_ammo, ammount_mag,img)
+        {
+        }
+    }
+}
